da364af526c6befe07822f65eb25620815ad6ba7
matiasrivero-ctrl <matiasariveros.cinco@gmail.com>
2023-02-10 17:28:27 -0300

    createTask function done

--- contracts/TaskContract.sol
+++ contracts/TaskContract.sol
@@ -2,6 +2,33 @@
 
 pragma solidity 0.8.0;
 
-contract Task {
+contract TaskContract {
     uint public taskCounter = 0;
+
+    // Estilo de la tarea
+    struct Task {
+        uint id;
+        string title;
+        string description;
+        bool done;
+        uint createdAt;
+    }
+
+    // Mapping is a reference type as arrays and structs
+    mapping(uint256 => Task) public tasks;
+
+    // Crear tarea
+    function createTask(
+        string memory _title,
+        string memory _description
+    ) public {
+        taskCounter++;
+        tasks[taskCounter] = Task(
+            taskCounter,
+            _title,
+            _description,
+            false,
+            block.timestamp
+        );
+    }
 }
