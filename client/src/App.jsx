476faad5b1fa3cea3ed3f2876b8a27046109ed52
matiasrivero-ctrl <matiasariveros.cinco@gmail.com>
2023-02-24 23:33:33 -0300

    updating ethers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,18 @@
-import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
-import './App.css';
 
 function App() {
-  const requestAccount = async () => {
-    if (window.ethereum) {
-      console.log('detected');
-
-      try {
-        const accounts = await window.ethereum.request({
-          method: 'eth_requestAccounts',
-        });
-        console.log(accounts);
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      console.log('Metamask is not installed');
-    }
-  };
+  // Connecting to metamask
   const connectWallet = async () => {
-    if (typeof window.ethereum !== undefined) {
-      await requestAccount();
-
-      const provider = new ethers.providers.Web3Provier(window.ethereum);
-      console.log(provider);
-    }
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const accounts = await provider.send('eth_requestAccounts', []);
+    console.log(accounts);
   };
 
   return (
-    <div className="App">
+    <>
       <h1>Hola Mundo</h1>
-
-      <button onClick={connectWallet}>Connect Wallet</button>
-    </div>
+      <button onClick={connectWallet}>Connect to Metamask 🦊</button>
+    </>
   );
 }
 
